c8dde65db532aad93785bc8ea39b2f42af3e53cb
Hernan Saez <hhsaez@gmail.com>
2018-01-28 14:33:23 -0300

    Improved screening solutions

--- Screening/Task1.cpp
+++ Screening/Task1.cpp
@@ -4,7 +4,7 @@
 
 #include "../Utils.hpp"
 
-#include <set>
+#include <unordered_set>
 
 struct Point2D {
 	int x;
@@ -15,14 +15,14 @@ int solution( std::vector< Point2D > A )
 {
 	const int N = A.size();
 
-	std::set< double > angles;
+	std::unordered_set< double > angles;
 
 	for ( int i = 0; i < N; i++ ) {
 		auto x = A[ i ].x;
 		auto y = A[ i ].y;
 
 		double angle = std::atan2( y, x );
-		angles.insert( angle ); // << O(Nlog(N)) !!!!!
+		angles.insert( angle );
 	}
 	
 	return angles.size();
@@ -37,6 +37,14 @@ int main( int argc, char **argv )
 {
 	test( { { -1, -2 }, { 1, 2 }, { 2, 4 }, { -3, 2 }, { 2, -2 } }, 4 );
 	test( { { -1000000000, -1000000000 }, { 1000000000, -1000000000 } }, 2 );
+	test( { { 1, 1 } }, 1 );
+	test( { { 1, 0 } }, 1 );
+	test( { { 0, 1 } }, 1 );
+	test( { { 1, 1 }, { -1, -1 } }, 2 );
+	test( { { 1, 0 }, { -1, 0 } }, 2 );
+	test( { { 0, 1 }, { 0, -1 } }, 2 );
+	test( { { 1, 1 }, { 1, 1, } }, 1 );
+	
 	return 0;
 }
 
--- Screening/Task3.cpp
+++ Screening/Task3.cpp
@@ -1,69 +1,95 @@
 /*
 
+There are N giraffes in your zoo. The zoo is famous for the distinct heights of its
+giraffes. This means that there are no two giraffes of equal height in the zoo.
+
+The giraffes live in displays located along the main path. Every giraffe has its own display.
+For aesthetic reasons, you want the giraffes to be arranged in ascending order of height. 
+Reordering all the giraffes at once, however, would cause the animals a lot of stress, so you
+want to split the displays into as many smaller groups as possible and reorder all the
+groups separately. Agroup should contain one or more consecutive displays. After 
+reordering all the groups, the giraffes should be arranged in ascending order of height.
+
+Write a function: 
+
+int solution( std::vector< int > &A );
+
+that, given a zero-indexed array A containing N integer, where A[ i ] denotes the height
+of the giraffe in the I-th display, returns the maxim number of groups that can be reordered
+independently in order to sort all the giraffes in ascending order of height.
+
+For example, given A = [1, 5, 4, 9, 8, 7, 12, 13, 14], the function should return 6. Displays
+can be split into six groups: [1], [5, 4], [9, 8, 7], [12], [13], [14]. Then, after 
+reordering each group, the whole sequence of giraffes will be sorted into ascending order.
+Note that group [12, 13, 14] was already ordered correctly, so it could be split into three
+groups, each containing one giraffe.
+
+Given A = [4, 3, 2, 6, 1], the function should return 1. Displays cannot be split into smaller
+groups; the giraffes have to be sorted all at once.
+
+Assume that:
+
+* N is an integer within the range [1..100000];
+* each element of array A is an integer within the range [1..1000000000];
+* the elements of A are all distinct.
+
+Complexity:
+
+* expected worst-case time complexity is O(N*log(N));
+* expected worst-case space complexity is O(N), beyond input storage (not counting
+the storage required for input arguments).
+
  */
 
 #include "../Utils.hpp"
+
+#include <stack>
 
 int solution( std::vector< int > A )
 {
 	const long N = A.size();
 
-	std::vector< int > indices( N );
+	std::stack< int > groups;
+
 	for ( int i = 0; i < N; i++ ) {
-		indices[ i ] = i;
-	}
-	std::sort( std::begin( indices ), std::end( indices ), [&A]( auto &left, auto &right ) {
-		return A[ left ] < A[ right ];
-	});
-
-	int next = 0;
-	int res = 0;
-
-	while ( next < N ) {
-		res++;
-		next = indices[ next ] + 1;
-	}
-		
-	return res;
-}
-
-int simple( std::vector< int > A )
-{
-	const int N = A.size();
-
-	int start = 0;
-	int end = 0;
-
-	int res = 0;
-
-	while ( start < N && end < N ) {
-		int min = std::numeric_limits< int >::max();
-		for ( int i = start; i < N; i++ ) {
-			if ( A[ i ] <= min ) {
-				min = A[ i ];
-				end = i;
-			}
+		if ( groups.empty() || A[ i ] > groups.top() ) {
+			// new group
+			groups.push( A[ i ] );
 		}
-
-		res++;
-		start = end + 1;
-		end = start;
+		else {
+			auto current = groups.top();
+			groups.pop();
+			while ( !groups.empty() && A[ i ] < groups.top() ) {
+				groups.pop();
+			}
+			groups.push( current );
+		}
 	}
-	
-	return res;
+
+	return groups.size();
 }
 
-void test( std::vector< int > A )
+void test( std::vector< int > A, int expected )
 {
-	codility::compareResults( solution( A ), simple( A ) );
+	codility::compareResults( solution( A ), expected );
 }
 
 int main( int argc, char **argv )
 {
-	test( { 1, 5, 4, 9, 8, 7, 12, 13, 14 } );
-	test( { 1, 5, 4, 9, 8, 7, 12, 13, 14, 2 } );
-	test( { 4, 3, 2, 6, 1 } );
-	test( { 1 } );
+	test( { 1, 5, 4, 9, 8, 7, 12, 13, 14 }, 6 );
+	test( { 1, 5, 4, 9, 8, 7, 12, 13, 14, 2 }, 2 );
+	test( { 4, 3, 2, 6, 1 }, 1 );
+	test( { 1, 2, 3, 4, 5, 6 }, 6 );
+	test( { 1 }, 1 );
+	test( {}, 0 );
+	test( { 1, 2, 3, 4 }, 4 );
+	test( { 4, 3, 2, 1 }, 1 );
+	test( { 5, 1, 2 }, 1 );
+	test( { 4, 5, 6, 10, 9, 8, 11, 7 }, 4 );
+
+	// should be 3, but input is invalid (different heights only)
+	test( { 1, 5, 5, 4 }, 2 ); 
+	
 	return 0;
 }
 
